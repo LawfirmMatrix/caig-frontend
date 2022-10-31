f2754d9e4e5895430e0a6cb790cdcbcad6b6b448
Joey <jgoldberg@lawfirmmatrix.com>
2022-10-31 17:24:23 -0400

    update backdrop on survey select

--- projects/survey/src/app/survey/components/backdrop/backdrop.component.ts
+++ projects/survey/src/app/survey/components/backdrop/backdrop.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HandsetComponent} from '../handset-component';
 import {shareReplay, switchMap} from 'rxjs/operators';
-import {map, filter} from 'rxjs';
-import {Survey, SurveyService} from '../../survey.service';
+import {map, filter, BehaviorSubject, combineLatest, Observable} from 'rxjs';
+import {Survey, SurveyService, SurveySchema} from '../../survey.service';
 import {BreakpointObserver} from '@angular/cdk/layout';
 import {TakeSurveyComponent} from '../take-survey/take-survey.component';
 import {ActivatedRoute} from '@angular/router';
@@ -13,12 +13,9 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['../styles.scss']
 })
 export class BackdropComponent extends HandsetComponent implements OnInit {
-  public surveys$ = this.dataService.get().pipe(shareReplay())
-  public schema$ = this.surveys$.pipe(
-    map((surveys) => surveys[0]),
-    filter((survey): survey is Survey => !!survey),
-    switchMap((survey) => this.dataService.getOneSchema(survey.schemaId)),
-  );
+  private static readonly SURVEY_ID = 'id';
+  public surveyId$ = new BehaviorSubject<string | undefined>(undefined);
+  public schema$!: Observable<SurveySchema>;
   public showLocation!: boolean;
   constructor(
     protected override breakpointObserver: BreakpointObserver,
@@ -28,9 +25,22 @@ export class BackdropComponent extends HandsetComponent implements OnInit {
     super(breakpointObserver);
   }
   public ngOnInit() {
-    this.showLocation = !!this.route.firstChild?.snapshot.params['id'];
+    const surveys$ = this.dataService.get().pipe(shareReplay());
+    this.schema$ = combineLatest([surveys$, this.surveyId$])
+      .pipe(
+        map(([surveys, surveyId]) => {
+          if (!surveyId) {
+            return surveys[0];
+          }
+          return surveys.find((s) => s.id === surveyId);
+        }),
+        filter((survey): survey is Survey => !!survey),
+        switchMap((survey) => this.dataService.getOneSchema(survey.schemaId))
+      );
+    this.showLocation = !!this.route.firstChild?.snapshot.params[BackdropComponent.SURVEY_ID];
   }
   public onActivate(event: any): void {
     this.showLocation = event instanceof TakeSurveyComponent;
+    this.surveyId$.next(this.route.firstChild?.snapshot.params[BackdropComponent.SURVEY_ID]);
   }
 }
