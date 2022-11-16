79363f0988e6c62052b2a2998267a313fd1f39d1
Joey <jgoldberg@lawfirmmatrix.com>
2022-11-15 22:36:37 -0500

    preserve query params on auto redirect

--- projects/survey/src/app/survey/guards/multiple-locations.guard.ts
+++ projects/survey/src/app/survey/guards/multiple-locations.guard.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, NavigationExtras} from '@angular/router';
 import {Observable, map, tap} from 'rxjs';
 import {SurveyDataService} from '../survey-data.service';
 
@@ -10,7 +10,7 @@ export class MultipleLocationsGuard implements CanActivate {
     return this.surveyService.initialize$
       .pipe(
         tap((survey) => {
-          const extras = { replaceUrl: true };
+          const extras: NavigationExtras = { replaceUrl: true, queryParamsHandling: 'preserve' };
           if (survey.locations?.length === 1) {
             this.router.navigate(['/survey', survey.id, survey.locations[0].id], extras);
           } else if (!survey.locations?.length) {
--- projects/survey/src/app/survey/guards/shortcut-redirect.guard.ts
+++ projects/survey/src/app/survey/guards/shortcut-redirect.guard.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, NavigationExtras} from '@angular/router';
 import {Observable, map, tap} from 'rxjs';
 import {SurveyDataService} from '../survey-data.service';
 
@@ -10,7 +10,7 @@ export class ShortcutRedirectGuard implements CanActivate {
     return this.surveyService.initialize$
       .pipe(
         tap((survey) => {
-          const extras = { replaceUrl: true };
+          const extras: NavigationExtras = { replaceUrl: true, queryParamsHandling: 'preserve' };
           const shortcut = route.paramMap.get('shortcut');
           const baseUrl = '/';
           if (!shortcut) {
