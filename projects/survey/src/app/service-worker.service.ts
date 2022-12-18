860b0e2514089365923f5011e17f06a10d22302c
Joey <jgoldberg@lawfirmmatrix.com>
2022-12-18 04:17:59 -0500

    fix hard refresh on survey app

--- projects/survey/src/app/service-worker.service.ts
+++ projects/survey/src/app/service-worker.service.ts
@@ -5,11 +5,20 @@ import {SwUpdate} from "@angular/service-worker";
 
 @Injectable({providedIn: 'root'})
 export class ServiceWorkerService {
+  private static readonly NO_SW_CONTROLLER = 'NO_SW_CONTROLLER';
   constructor(private updates: SwUpdate) { }
   public initialize(): Observable<any> {
     if (!this.updates.isEnabled) {
       return of(null);
     }
+    if (!navigator.serviceWorker.controller) {
+      const alreadyReloaded = localStorage.getItem(ServiceWorkerService.NO_SW_CONTROLLER);
+      if (!alreadyReloaded) {
+        localStorage.setItem(ServiceWorkerService.NO_SW_CONTROLLER, 'true');
+        location.reload();
+      }
+    }
+    localStorage.removeItem(ServiceWorkerService.NO_SW_CONTROLLER);
     return from(this.updates.checkForUpdate())
       .pipe(
         tap((updateFound) => {
