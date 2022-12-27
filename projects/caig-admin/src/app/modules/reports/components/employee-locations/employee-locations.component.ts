dcc917f33e0b07dbb61906374565301fb17a2da4
Joey <jgoldberg@lawfirmmatrix.com>
2022-12-27 06:21:09 -0500

    markers

--- projects/caig-admin/src/app/modules/reports/components/employee-locations/employee-locations.component.ts
+++ projects/caig-admin/src/app/modules/reports/components/employee-locations/employee-locations.component.ts
@@ -1,8 +1,24 @@
 import {Component} from '@angular/core';
+import {Map, marker, Marker} from 'leaflet';
 
 @Component({
   selector: 'app-employee-locations',
   templateUrl: './employee-locations.component.html',
   styleUrls: ['./employee-locations.component.scss']
 })
-export class EmployeeLocationsComponent { }
+export class EmployeeLocationsComponent {
+  public map!: Map;
+  public markers: Marker[] = [];
+  public addMarkers(): void {
+    this.clearMarkers();
+    for (let i = 0; i < 200; i++) {
+      const m = marker([Math.random() * 180 - 90, Math.random() * 360 - 180]);
+      this.markers.push(m);
+      m.addTo(this.map);
+    }
+  }
+  public clearMarkers(): void {
+    this.markers.forEach((m) => this.map.removeLayer(m));
+    this.markers = [];
+  }
+}
--- projects/caig-admin/src/app/modules/shared/world-map/world-map.component.ts
+++ projects/caig-admin/src/app/modules/shared/world-map/world-map.component.ts
@@ -1,4 +1,4 @@
-import {Component, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
+import {Component, AfterViewInit, ViewChild, ElementRef, Output, EventEmitter} from '@angular/core';
 import {Browser, Map, map, tileLayer, TileLayerOptions} from 'leaflet';
 
 @Component({
@@ -7,6 +7,8 @@ import {Browser, Map, map, tileLayer, TileLayerOptions} from 'leaflet';
   styleUrls: ['./world-map.component.scss']
 })
 export class WorldMapComponent implements AfterViewInit {
+  @Output() public mapCreated = new EventEmitter<Map>();
+
   @ViewChild('map') public mapContainer!: ElementRef<HTMLElement>;
 
   public ngAfterViewInit() {
@@ -14,13 +16,16 @@ export class WorldMapComponent implements AfterViewInit {
     const baseUrl = 'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}';
     const regUrl = `${baseUrl}.png?apiKey=${apiKey}`;
     const retinaUrl = `${baseUrl}@2x.png?apiKey=${apiKey}`;
-    const lefletMap: Map = map(this.mapContainer.nativeElement).setView([39.369, -76.717], 12);
+    const leafletMap: Map = map(this.mapContainer.nativeElement).setView([39.369, -76.717], 3);
 
     tileLayer(Browser.retina ? retinaUrl : regUrl, {
       attribution: 'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | <a href="https://openmaptiles.org/" target="_blank">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap</a> contributors',
       apiKey,
       maxZoom: 20,
       id: 'osm-bright',
-    } as TileLayerOptions).addTo(lefletMap);
+      className: 'map-tiles'
+    } as TileLayerOptions).addTo(leafletMap);
+
+    this.mapCreated.emit(leafletMap);
   }
 }
